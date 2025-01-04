1cad367d129ddb4c409eec9ff2a0a15595b3e0c8
andreeezinho <andreeesapucaia@gmail.com>
2025-01-04 19:38:16 -0300

    created work_load model

---
+++ src/Models/Work_Load/Carga_Horaria.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models\Work_Load;
+
+use App\Models\Traits\UuidTrait;
+
+class Carga_Horaria{
+
+    use UuidTrait;
+
+    public $id;
+    public $uuid;
+    public $carga;
+    public $ativo;
+    public $created_at; 
+    public $updated_at;
+
+    public function __construct(){}
+
+    public function create(
+        array $data
+    ): Carga_Horaria {
+        $carga_horaria->id = $data['id'] ?? null;
+        $carga_horaria->uuid = $data['uuid'] ?? $this->generateUUID();
+        $carga_horaria->carga = $data['carga'] ?? null;
+        $plan->created_at = $data['created_at'] ?? null;
+        $plan->updated_at = $data['updated_at'] ?? null;
+        return $carga_horaria;
+    }
+
+}
