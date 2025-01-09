9ecc93866db5d83220ea8c6af8b7fa6055a76c7e
KEWEN SILVA <kewensilva58@gmail.com>
2025-01-09 13:29:07 -0300

    [fix: coordenação model]

--- src/Models/Coordination/Coordenacao.php
+++ src/Models/Coordination/Coordenacao.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models\Classrooms;
+namespace App\Models\Coordination;
 
 use App\Models\Traits\UuidTrait;
 
@@ -8,29 +8,27 @@ class Coordenacao {
     
     use UuidTrait;
 
-    protected $id;
-    protected $uuid;
-    protected $pessoa_fisica_id;
-    protected $graduacao;
-    protected $ativo;
-    protected $created_at;
-    protected $updated_at;
+    public $id;
+    public $uuid;
+    public $person_id;
+    public $graduacao;
+    public $ativo;
+    public $created_at;
+    public $updated_at;
 
-    public function __construct (array $data = []) {
-        if(!empty($data)) {
-            $this->id = $data['id'] ?? null;
-            $this->uuid = $data['uuid'] ?? $this->generateUUID();
-            $this->pessoa_fisica_id = $data['pessoa_fisica_id'] ?? null;          
-            $this->graduacao = $data['graduacao'];  
-            $this->ativo = $data['active'] ?? null;  
-            $this->created_at = $data['created_at'] ?? null;
-            $this->updated_at = $data['updated_at'] ?? null;
-        }
-    }
+    public function __construct () {}
 
     public function create(
         array $data
-    ): self {
-        return new self($data);
+    ): Coordenacao {
+        $coordination = new Coordenacao();
+        $coordination->id = $data['id'] ?? null;
+        $coordination->uuid = $data['uuid'] ?? $this->generateUUID();
+        $coordination->pessoa_fisica_id = $data['person_id'] ?? null;          
+        $coordination->graduacao = $data['graduacao'];  
+        $coordination->ativo = $data['active'] ?? null;  
+        $coordination->created_at = $data['created_at'] ?? null;
+        $coordination->updated_at = $data['updated_at'] ?? null;
+        return $coordination;
     }
 }
