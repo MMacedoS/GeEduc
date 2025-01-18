de3b105ec7caba2e00bf6ccdd500622a1ce07f67
KEWEN SILVA <kewensilva58@gmail.com>
2025-01-18 17:41:34 -0300

    :hammer: [Feat: Adiciona metodos indexTeacher e store na controller de notas]

--- src/Controllers/v1/Scores/NotaController.php
+++ src/Controllers/v1/Scores/NotaController.php
@@ -7,19 +7,17 @@ use App\Repositories\Activitie\AtividadeRepository;
 use App\Repositories\Bimester\BimestreRepository;
 use App\Repositories\Classrooms\TurmaDisciplinaRepository;
 use App\Repositories\Frequencies\FrequenciaRepository;
+use App\Repositories\Scores\NotaRepository;
 use App\Repositories\Student\EstudanteTurmaRepository;
 use App\Repositories\Teacher\ProfessorDisciplinaRepository;
 use App\Request\Request;
-use App\Utils\LoggerHelper;
-use App\Utils\Paginator;
-use App\Utils\Validator;
 
 class NotaController extends Controller 
 {
-    const TEN = 10;
     protected $atividadeRepository;
     protected $turmaDisciplinaRepository;
     protected $frequenciaRepository;
+    protected $notaRepository;
     protected $estudanteTurmaRepository;
     protected $professorDisciplinaRepository;
     protected $bimestreRepository;
@@ -33,42 +31,18 @@ class NotaController extends Controller
         $this->estudanteTurmaRepository = new EstudanteTurmaRepository();
         $this->professorDisciplinaRepository = new ProfessorDisciplinaRepository();
         $this->bimestreRepository = new BimestreRepository();
-    }
-
-    public function indexStudents(Request $request, string $class_discipline_id)
-    {
-        $student_class = $this->estudanteTurmaRepository->findByUuid($class_discipline_id);
-        
-        $frequencias = $this->frequenciaRepository
-            ->allFrequencies(
-                [
-                    'student_id' => $student_class->id,
-                    'class_id' => $student_class->turma_id
-                ]
-            );
-
-        $perPage = 10;
-        $currentPage = $request->getParam('page') ? (int)$request->getParam('page') : 1;
-        $paginator = new Paginator($frequencias, $perPage, $currentPage);
-        $paginatedBoards = $paginator->getPaginatedItems();
-
-        return $this->router->view(
-            'student/my-classrooms/frequencies', 
-            [
-                'active' => 'students',
-                'estudante_turma' => $student_class,
-                'frequencias' => $paginatedBoards, 
-                'links' => $paginator->links()
-            ]
-        ); 
+        $this->notaRepository = new NotaRepository();
     }
 
     public function indexTeacher(Request $request, string $class_discipline_id)
     {
+        $paramsURL = $request->getQueryParams();
+     
         $turma_disciplina = $this->turmaDisciplinaRepository
             ->allClassDisciplines(
                 ['uuid' => $class_discipline_id]
             )[0];
+ 
 
         $estudantes = $this->estudanteTurmaRepository
             ->allClassStudents(
@@ -77,15 +51,15 @@ class NotaController extends Controller
                     'school_year' => Date('Y')
                 ]
             );
+            
 
-        $frequencias = $this->frequenciaRepository
-            ->allFrequencies(
-                [
-                    'class_discipline_id' => $turma_disciplina->id,
-                    'class_id' => $turma_disciplina->turma_id
-                ]
-            );
-
+        $atividades = $this->atividadeRepository->allActivities(['class_discipline_id' => $turma_disciplina->id]);
+        
+        $notas = $this->notaRepository->allScores([
+            'class_discipline_id' => $turma_disciplina->id, 
+            'bimester_id' => $paramsURL['bimester_id'] ?? null
+        ]);
+        
         $bimestres = $this->bimestreRepository->allBimesters();
 
         return $this->router->view(
@@ -94,40 +68,33 @@ class NotaController extends Controller
                 'active' => 'teacher',
                 'turma_disciplina' => $turma_disciplina,
                 'estudantes' => $estudantes,
-                'frequencias' => $frequencias,
-                'bimestres' => $bimestres
+                'notas' => $notas,
+                'bimestres' => $bimestres,
+                'atividades' => $atividades,
+                'bimestreFilter' => $paramsURL['bimester_id'] ?? null,
             ]
-        ); 
+        );
     }
 
     public function store(Request $request, string $class_discipline_id)
     {
         $data = $request->getBodyParams();
-
-        $turma_disciplina = $this->turmaDisciplinaRepository->findByUuid($class_discipline_id);
-
-        $data['class_discipline_id'] = $turma_disciplina->id;
-
-        foreach ($data['class_student_id'] as $value) {
-            $data['class_student_id'] = $value;
-            
-            $created = $this->frequenciaRepository->create($data);
+       
+        if (isset($data['notas'])) {
+            foreach ($data['notas'] as $scoreAndActivitieID => $score) {
+                $arrayScoreAndActivitieID = explode(',', $scoreAndActivitieID, 2);
+                $data['nota'] = $score;
+                $data['estudante_turma_id'] = $arrayScoreAndActivitieID[0];
+                $data['atividade_id'] = $arrayScoreAndActivitieID[1];
+                
+                $created = $this->notaRepository->create($data);
+            }
         }
-        
+
         if(is_null($created)){
-            return $this->router->redirect("meus-componentes/$class_discipline_id/frequencia?error=422");
+            return $this->router->redirect("meus-componentes/$class_discipline_id/notas?error=422");
         }
-        
-        return $this->router->redirect("meus-componentes/$class_discipline_id/frequencia");
+       
+        return $this->router->redirect("meus-componentes/$class_discipline_id/notas?bimester_id=$data[bimester_id]");
     }
-
-    // $dataForChart = array_map(function ($frequencia) {
-    //     return [
-    //         'y' => date('Y-m-d', strtotime($frequencia->data)), // Formatar a data
-    //         'frequencia' => intval($frequencia->frequencia), // Frequência como valor numérico
-    //     ];
-    // }, $frequencias);
-    
-    // // Exemplo de saída JSON para usar no gráfico
-    // echo json_encode($dataForChart, JSON_PRETTY_PRINT);
 }
