fd78d57ca30bc4dbfd293d0d98af347fc389197a
andreeezinho <andreeesapucaia@gmail.com>
2025-01-20 10:43:22 -0300

    created site_carousel model

---
+++ src/Models/SiteCarousel/SiteCarrossel.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models\SiteCarousel;
+
+use App\Models\Traits\UuidTrait;
+
+class SiteCarrossel {
+
+    use UuidTrait;
+
+    public $id;
+    public $uuid;
+    public $nome;
+    public $descricao;
+    public $site_arquivo_id;
+    public $local;
+    public $ativo;
+    public $link;
+    public $created_at;
+    public $updated_at;
+
+    public function __construct(){}
+
+    public function create(
+        array $data
+    ) : SiteCarrossel {
+        $site_carousel = new SiteCarrossel();
+        $site_carousel->id = $data['id'] ?? null;
+        $site_carousel->uuid = $data['uuid'] ?? $this->generateUUID();
+        $site_carousel->nome = $data ['name'] ?? null;
+        $site_carousel->descricao = $data ['description'] ?? null;
+        $site_carousel->site_arquivo_id = $data['site_arquivo_id'];
+        $site_carousel->local = $data['local'] ?? null;
+        $site_carousel->ativo = $data['active'] ?? null;
+        $site_carousel->link = $data['link'] ?? null;
+        $site_carousel->created_at = $data['created_at'] ?? null;
+        $site_carousel->updated_at = $data['updated_at'] ?? null;
+        return $site_carousel;
+    }
+
+}
