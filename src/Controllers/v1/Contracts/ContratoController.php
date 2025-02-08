ac28831af7f520364dd63151ea81660009109d90
KEWEN SILVA <kewensilva58@gmail.com>
2025-02-08 18:33:23 -0300

    :hammer: [Feat: insere o metodo para gerar e enviar os contratos]

--- src/Controllers/v1/Contracts/ContratoController.php
+++ src/Controllers/v1/Contracts/ContratoController.php
@@ -2,18 +2,39 @@
 namespace App\Controllers\v1\Contracts;
 
 use App\Controllers\Controller;
+use App\Interfaces\Student\IEstudanteMensalidadeRepository;
 use App\Services\AutentiqueService;
+use App\Controllers\v1\Traits\ManipulationPDF;
 
 class ContratoController extends Controller {
+  use ManipulationPDF;
   protected $autentiqueService;
-  public function __construct() {
+  protected $estudanteMensalidadeRepository;
+
+  public function __construct(
+    IEstudanteMensalidadeRepository $estudanteMensalidadeRepository
+  )
+  {
     parent::__construct();
+
+    $this->estudanteMensalidadeRepository = $estudanteMensalidadeRepository;
     $this->autentiqueService = new AutentiqueService();
   }
 
   public function index() {
-    $this->autentiqueService->listarEstudantesMensalidades();
-    dd("da");
-    return $this->router->view("contracts/contrato");
+    $students = $this->estudanteMensalidadeRepository->allMonthlyfees();
+    // dd($students);
+    $this->generateAndSendContracts($students);
+    // dd("chega");
+    return $this->router->view("contracts/index");
+  }
+
+  public function generateAndSendContracts($students) {
+    foreach($students as $student) {
+      $pathPDF = $this->generatePDF($student, "/Resources/Views/contracts/contrato.php", "/Resources/Views/contracts/contrato");
+      if($pathPDF) {
+        $this->autentiqueService->sendContract($student, $pathPDF);
+      }
+    }
   }
 }
