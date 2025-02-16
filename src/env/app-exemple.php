1c8714fb1994ff882feecd8fd11e4cb5657b87a0
Mauricio Macedo da Silva <mauricio.silva@fepese.org.br>
2025-02-16 10:09:52 -0300

    feat: code adjusted

---
+++ src/env/app-exemple.php
@@ -0,0 +1,10 @@
+<?php
+
+const APP_NAME = 'GeEduc - Gestor Educacional';
+const URL_PREFIX_APP = "http://localhost:8080";
+const DB_HOST = 'mysql';
+const DB_NAME = 'banco';
+const DB_USER = 'root';
+const DB_PASS = '12345';
+const NAME_SCHOOL = "Instituto Social de Tucano";
+const ACRONYM_SCHOOL = "IST";
